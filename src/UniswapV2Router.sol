2305f90b6964ba59cb9b889ecc1e4002db6484df
frogmonkee <118191517+frogmonkee-uniswap@users.noreply.github.com>
2023-08-21 21:07:14 -0400

    Cleaning up

--- src/UniswapV2Router.sol
+++ src/UniswapV2Router.sol
@@ -30,6 +30,7 @@ contract Uniswapv2PairRouter {
         if (factory.pairs(tokenA, tokenB) == address(0)) {
             factory.createPair(tokenA, tokenB);
         }
+        // Returns numebr of tokenA and tokenB we need to deposit according to x*y=k
         (amountA, amountB) = _calculateLiquidity(
             tokenA,
             tokenB,
@@ -38,6 +39,7 @@ contract Uniswapv2PairRouter {
             amountAMin,
             amountBMin);
         address pairAddress = UniswapV2Library.pairFor(address(factory), tokenA, tokenB);
+        // In UniswapV2Pair.sol, the user has to manually do this. Routing contract transfers automatically
         _safeTransferFrom(tokenA, msg.sender, pairAddress, amountA);
         _safeTransferFrom(tokenB, msg.sender, pairAddress, amountB);
         liquidity = IUniswapV2Pair(pairAddress).mint(to);
@@ -46,13 +48,19 @@ contract Uniswapv2PairRouter {
     function _calculateLiquidity(
         address tokenA,
         address tokenB,
+        // Amount of token A & token B we want to deposit. Upper bound.
+        // This is in place of the user manualy transfering tokens into the pair contract (see SwapTest.t.sol)
         uint256 amountADesired,
         uint256 amountBDesired,
+        // Minimum amounts of tokens A & B we want to deposit. The lower amount determines LP tokens
+        // See `mint` function in UniswapV2Pair.sol
         uint256 amountAMin,
         uint256 amountBMin) internal returns(
             uint256 amountA,
             uint256 amountB
         ) {
+            // Need to call library for getReserves instead of directly in UniswapV2Pair.sol contract bc...
+            // ... the pair address is not known
             (uint256 reserveA, uint256 reserveB) = UniswapV2Library.getReserves(
                 address(factory),
                 tokenA,
@@ -63,20 +71,26 @@ contract Uniswapv2PairRouter {
                 // Our newly deposited liquidity will define reserve ratios
                 (amountA, amountB) = (amountADesired, amountBDesired);
             } else {
+                // AmountB we need to add if we're depositing AmountADesired
                 uint256 amountBOptimal = UniswapV2Library.getQuote(
                     amountADesired,
                     reserveA,
                     reserveB
                 );
+                // If AmountBOptimial is less than the amountB we're depositing
+                // Then only deposit the minimum amountB required (optimal, not desired)
                 if(amountBOptimal <= amountBDesired) {
                     if (amountBOptimal <= amountBMin) revert InsufficientBAmount();
                     (amountA, amountB) = (amountADesired, amountBOptimal);
                 } else {
+                    // AmountA we need to add if we're depositing AmountBDesired
                     uint256 amountAOptimal = UniswapV2Library.getQuote(
                         amountBDesired,
                         reserveB,
                         reserveA
                     );
+                    // If this line executes, then amountBOptimal is more than what we are depositing (amountBDesired)
+                    // If AmountA has the same problem, then we need to revert. Otherwise user will lose funds
                     assert(amountAOptimal <= amountADesired);
                     if (amountAOptimal <= amountAMin) revert InsufficientAAmount();
                     (amountA, amountB) = (amountAOptimal, amountBDesired);
--- src/libraries/UniswapV2Library.sol
+++ src/libraries/UniswapV2Library.sol
@@ -14,11 +14,14 @@ library UniswapV2Library {
         address tokenA,
         address tokenB
     ) public returns (uint256 reserveA, uint256 reserveB) {
+        // Creates local variables for tokens sorted such that token0 > token1
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         (uint256 reserve0, uint256 reserve1) = IUniswapV2Pair(pairFor(factoryAddress, token0, token1)).getReserves();
+        // Verifies that the token0 address matches tokenA. Otherwise the order is reversed
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
+    // Standardizes storage mapping such that the less token address is mapped to greater token address
     function sortTokens(address tokenA, address tokenB) internal pure returns(address token0, address token1) {
         return tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
     }
