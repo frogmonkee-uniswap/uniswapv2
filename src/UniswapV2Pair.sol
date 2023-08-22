aeee39e5987c0c7c53a1333efd70f46e6b78a45b
frogmonkee <118191517+frogmonkee-uniswap@users.noreply.github.com>
2023-08-22 16:05:38 -0400

    Clean up

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -7,7 +7,6 @@ import "solmate/tokens/ERC20.sol";
 interface IERC20 {
   function balanceOf(address) external returns (uint256);
   function transfer(address to, uint256 amount) external;
-  function approve(address, uint256) external returns(bool);
 }
 
 error InsufficientLiquidityMinted();
